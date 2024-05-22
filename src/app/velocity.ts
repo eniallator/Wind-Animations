39c245fc5ddff254b3332105752e255c13bbc3ab
eniallator <eniallatormc@gmail.com>
2024-05-22 19:22:10 +0100

    Made the curves scale with the min screen size

--- src/app/velocity.ts
+++ src/app/velocity.ts
@@ -6,30 +6,34 @@ const GOLDEN_RATIO = (1 + Math.sqrt(5)) / 2;
 
 const vortex: VelocityFunc = ({ canvas, time, paramConfig }) => {
   const center = Vector.create(canvas.width / 2, canvas.height / 2);
-  const speed = 0.1 + paramConfig.getVal("speed") / (1 / 0.9);
 
   const timeSoFar = time.now - time.animationStart;
   const steppedTime = 2 * timeSoFar - Math.floor(timeSoFar / 8);
 
+  const multiplier =
+    ((0.1 + paramConfig.getVal("speed") / (1 / 0.9)) *
+      Math.min(canvas.width, canvas.height)) /
+    1000;
   return vec => {
     const diff = vec.copy().sub(center).divide(center);
     const c = diff.getAngle() - steppedTime;
 
     return Vector.create(
-      2 * speed * Math.cos(diff.getMagnitude() / (2 * time.delta) + c),
-      speed *
+      2 * multiplier * Math.cos(diff.getMagnitude() / (2 * time.delta) + c),
+      multiplier *
         Math.sin(diff.getMagnitude() / (2 * GOLDEN_RATIO * time.delta) + c)
     );
   };
 };
 
-const sweepingRight: VelocityFunc = ({ time, paramConfig }) => {
+const sweepingRight: VelocityFunc = ({ time, paramConfig, canvas }) => {
   const sizeVec = Vector.create(
     500 * (paramConfig.getVal("speed") + 0.1) * time.delta,
     0
   );
+  const divisor = (Math.min(canvas.width, canvas.height) * 1.2) ** 2 / 2;
   return vec =>
-    sizeVec.copy().setAngle(Math.cos(vec.getSquaredMagnitude() / 1e6));
+    sizeVec.copy().setAngle(Math.cos(vec.getSquaredMagnitude() / divisor));
 };
 
 const zigZag: VelocityFunc = ({ time, canvas, paramConfig }) => {
